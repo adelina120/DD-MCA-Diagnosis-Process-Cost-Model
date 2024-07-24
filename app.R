7ca6e248bfa3ec1da50bf35ba3d186ca5bab6623
stormliucong <stormliucong@gmail.com>
2024-07-24 15:44:02 -0400

    add cma yield parameter & add reset button

--- app.R
+++ app.R
@@ -6,27 +6,29 @@ library(DT)
 # Define the parameter lookup dictionary
 if (!file.exists("parameter_lookup.csv")) {
   pt <- data.frame(
-    id = c(1:18),
+    id = c(1:19),
     name = c("CMA_cost", "GP_cost", "ES_cost", "GP_post_n", "GP_post_p", "ES_post_n", "ES_post_p",
-             "penalty", "GP_yield", "ES_yield", "expert_GP_tn", "expert_GP_tp",
+             "penalty", "CMA_yield", "GP_yield", "ES_yield", "expert_GP_tn", "expert_GP_tp",
              "expert_ES_tn", "expert_ES_tp", "ai_GP_tn", "ai_GP_tp", "ai_ES_tn", "ai_ES_tp"),
     display_name = c("CMA test cost", "Gene-panel test cost", "Exome sequencing test cost",
                      "Gene-panel post-test cost(negative)", "Gene-panel(positive)",
-                     "Exome Sequencing(negative)", "Exome Sequencing(positive)", "FN penalty",
-                     "Gene-panel", "Exome Sequencing", "Expert TN rate (GP)", "Expert TP rate (GP)",
+                     "Exome Sequencing(negative)", "Exome Sequencing(positive)", "FN penalty","CMA yield",
+                     "Gene-panel yield", "Exome Sequencing yield", "Expert TN rate (GP)", "Expert TP rate (GP)",
                      "Expert TN rate (ES)", "Expert TP rate (ES)", "AI TN rate (GP)", "AI TP rate (GP)",
                      "AI TN rate (ES)", "AI TP rate (ES)"),
-    default_value = c(500, 1500, 3000, 500, 1000, 1500, 1500, 2000, 0.2, 0.3, 0.6, 0.65, 0.7, 0.75, 0.6, 0.65, 0.7, 0.75),
+    default_value = c(500, 1500, 3000, 500, 1000, 1500, 1500, 2000, 0.05, 0.2, 0.3, 0.6, 0.65, 0.7, 0.75, 0.6, 0.65, 0.7, 0.75),
     description = c("Description of param1", "Description of param2", "Description of param3",
                     "Description of param4", "Description of param5", "Description of param6",
                     "Description of param7", "Description of param8", "Description of param9",
                     "Description of param10", "Description of param11", "Description of param12",
                     "Description of param13", "Description of param14", "Description of param15",
-                    "Description of param16", "Description of param17", "Description of param18"),
+                    "Description of param16", "Description of param17", "Description of param18",
+                    "Description of param19"),
     
-    group = c(rep("Cost", 8), rep("Diagnostic Yield", 2), rep("Expert Performance", 4), rep("AI Performance", 4)),
+    group = c(rep("Cost", 8), rep("Diagnostic Yield", 3), rep("Expert Performance", 4), rep("AI Performance", 4)),
     stringsAsFactors = FALSE
   )
+  write.csv2(pt, "parameter_lookup.csv")
 } else {
   pt <- read.csv2("parameter_lookup.csv", stringsAsFactors = FALSE)
 }
@@ -51,6 +53,7 @@ cost <- function(CMA, GP, ES, GP_post_n, GP_post_p, ES_post_n, ES_post_p, penalt
 
 plotBarPlot <-function(input){
   # Create a plot
+  # implement business logic here
   plot(1:10, 1:10, type = "n", xlab = input$xaxis, ylab = input$yaxis)
 }
 
@@ -58,7 +61,7 @@ generateParameterTable <-function(input){
   working_pt = pt 
   working_pt$default_value = NULL
   value = c(input$CMA_cost, input$GP_cost, input$ES_cost, input$GP_post_n, input$GP_post_p, input$ES_post_n, input$ES_post_p,
-            input$penalty, input$GP_yield, input$ES_yield, input$expert_GP_tn, input$expert_GP_tp,
+            input$penalty, input$CMA_yield, input$GP_yield, input$ES_yield, input$expert_GP_tn, input$expert_GP_tp,
             input$expert_ES_tn, input$expert_ES_tp, input$ai_GP_tn, input$ai_GP_tp, input$ai_ES_tn, input$ai_ES_tp)
   working_pt$value = value
   working_pt
@@ -69,20 +72,19 @@ ui <- fluidPage(
   titlePanel("Cost Function Visualization"),
   # Add some custermized CSS styles
   
-  # Add a description
   sidebarLayout(
     sidebarPanel(
-      # Drop Down Menu for selecting y-axis. only one option can be selected for now
       fluidRow(
         column(12,
                div(
                  h4("X and Y axis"),
+                 # Drop Down Menu for selecting y-axis. only one option can be selected for now
                  selectInput("yaxis", "Y-axis",
                              choices = c("Expected Cost"),
                              selected = "Expected Cost"),
                  selectInput("xaxis", "X-axis",
                              choices = c("Branch 1", "Branch 2", "Branch 3"),
-                             selected = "Branch 1"),
+                             selected = "Branch 1")
                  # add some annotation. The annotation will be shown when the mouse hover over the question mark
                  # bsPopover(id = "xaxis", title = NULL, content = "Branch 1: start with ES; Branch 2: start with ES", placement = "bottom", trigger = "hover")
                )
@@ -97,6 +99,8 @@ ui <- fluidPage(
                )
         )
       ),
+      actionButton("reset", "Reset", class = "btn-warning"),
+      
       conditionalPanel(
         condition = "input.parameter_group == 'Cost'",
         fluidRow(
@@ -160,8 +164,6 @@ ui <- fluidPage(
           )
         )
       ),
-      
-      actionButton("reset", "Reset all parameters")
     ),
     mainPanel(
       h3("Introduction"),
@@ -173,7 +175,19 @@ ui <- fluidPage(
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+  # Reactive value to store the updated parameter lookup
+  param_data <- reactiveVal(pt)
+  
+  # Reset all parameters to their default values when the reset button is clicked
+  observeEvent(input$reset, {
+    param_data(pt)
+    # Reset input fields to their default values
+    for (i in 1:nrow(pt)) {
+      updateNumericInput(session, pt$name[i], value = pt$default_value[i], min=0)
+    }
+  })
+  
   # Define a reactive expression for the plot
   output$expected_cost <- renderPlot({
     plotBarPlot(input)
