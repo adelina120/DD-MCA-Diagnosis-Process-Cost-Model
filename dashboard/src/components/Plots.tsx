63bd780327e3832e89df807ec3880d8240f58475
Adelina Nie <152323838+adelina120@users.noreply.github.com>
2025-05-28 15:37:06 -0400

    Update Plots.tsx

--- dashboard/src/components/Plots.tsx
+++ dashboard/src/components/Plots.tsx
@@ -56,7 +56,7 @@ const Plots: React.FC<PlotsProps> = ({ data, plotConfig }) => {
       title: `${yAxis} vs ${xAxis}`,
       xaxis: { title: xAxis },
       yaxis: { title: yAxis },
-      font: {size: 18},
+      font: {size: 25},
       showlegend: true
     }
   };
